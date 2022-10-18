7e5f4de0d2a0d308c6dc50f3438e45516de94e7b
Matthieu <matthieu.rode@cae.com>
2022-10-18 15:03:23 -0400

    Fix initializer http request

--- src/utils/initialize.ts
+++ src/utils/initialize.ts
@@ -1,18 +1,17 @@
+import http from 'http';
+
 const stagingUrl = 'http://localhost:3000';
 const prodUrl = 'https://cdn.jsdelivr.net/npm/@cae-cobalt/cae-webflow-library@1/dist';
 
 export const initialize = (components: Array<string>) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', stagingUrl);
-  request.send();
-  request.onload = () => {
-    const isProduction = request.status === 200 || request.status === 301;
-    const baseUrl = isProduction ? prodUrl : stagingUrl;
+  http.get({ host: stagingUrl }, function (res) {
+    const isStaging = res.statusCode === 200 || res.statusCode === 301;
+    const baseUrl = isStaging ? stagingUrl : prodUrl;
     components.forEach((component) => {
       const url = new URL(`${baseUrl}/components/${component}.js`).toString();
       appendScript(url);
     });
-  };
+  });
 };
 
 function appendScript(url: string) {
@@ -22,4 +21,5 @@ function appendScript(url: string) {
   htmlEl.defer = true;
   document.body.append(htmlEl);
 }
+
 initialize(['tooltip']);
